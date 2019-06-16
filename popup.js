490a8878a482dc12a4e8f22709bfb84c0b272aca
Julio Cesar Fausto <jcf@jcfausto.com>
2019-06-16 18:44:44 +0200

    Applying the color to the web page

--- popup.js
+++ popup.js
@@ -5,3 +5,14 @@ let color = Math.floor(Math.random() * 16777215).toString(16);
 
 changeColor.style.backgroundColor = `#${color}`;
 changeColor.setAttribute('value', `#${color}`);
+
+// Step 5 - Applying the color to the page
+changeColor.addEventListener('click', (element) => {
+    let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: `document.body.style.backgroundColor = "${color}";`}
+        );
+    });
+});
